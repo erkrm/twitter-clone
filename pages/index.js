fa4ac88b5b339132d547eeb526916be74ec5ca27
unknown <erkroch@gmail.com>
2023-01-19 10:01:25 +0100

    create the widgets component and search bar

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import Sidebar from '@/components/Sidebar';
 import Feed from '@/components/Feed';
+import Widgets from '@/components/Widgets';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -25,6 +26,8 @@ export default function Home() {
         <Feed />
 
         {/** WIDGET */}
+
+        <Widgets />
 
         {/** MODAL */}
       </main>
